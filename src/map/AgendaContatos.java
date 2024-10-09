3a4ac2788d69cd818162377f621ed781814271a7
AmosRibeiro <amosribeiro2708@gmail.com>
2024-10-09 16:01:02 -0300

    uso de map

---
+++ src/map/AgendaContatos.java
@@ -0,0 +1,43 @@
+package map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AgendaContatos {
+    //atributo
+    private Map<String, Integer> agendaContatoMap;
+
+    public AgendaContatos() {
+        this.agendaContatoMap = new HashMap<>();
+    }
+
+    public void adicionarContato(String nome, Integer telefone){
+        agendaContatoMap.put(nome, telefone);
+    }
+
+    public void removerContato(String nome){
+        if(!agendaContatoMap.isEmpty()){
+            agendaContatoMap.remove(nome);
+        }
+    }
+
+    public void exibirContatos(){
+        System.out.println(agendaContatoMap);
+    }
+
+    public Integer pesquisarPornome(String nome){
+        Integer numeroPorNome = null;
+        if(!agendaContatoMap.isEmpty()){
+            agendaContatoMap.get(nome);
+        }
+        return numeroPorNome;
+    }
+
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.adicionarContato("Camila", 123333);
+
+        agendaContatos.exibirContatos();
+    }
+}
